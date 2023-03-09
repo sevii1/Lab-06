5711d2704383634a7573e61c552d8a3d00acb1b8
sevii1 <hbass1@ufl.edu>
2023-03-09 14:26:46 -0500

    Wrote encoder function

---
+++ main.py
@@ -0,0 +1,33 @@
+# Encoder
+def encode(phrase):
+    encoded_phrase = ""
+    if len(phrase) == 8:
+        for i in phrase:
+            encoded_phrase += str((int(i) + 3) % 10)
+    return encoded_phrase
+
+
+def decode(phrase):
+    pass
+
+
+def main():
+    # looping menu
+    option = '1'
+    phrase = ''
+    while option != '0':
+        # print menu
+        print('0. Exit')
+        print('1. Enter a new phrase')
+        print('2. Print encoded phrase')
+        print('3. Print decoded phrase')
+        option = input('Enter an option: ')
+
+        if option == '1':
+            phrase = input('Enter your phrase: ')
+        elif option == '2':
+            print('Encoded phrase is', encode(phrase))
+        elif option == '3':
+            print('Decoded phrase is', decode(phrase))
+
+main()
